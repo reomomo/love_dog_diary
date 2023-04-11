6d345fd98b2eda97086a7de05f1d3545599f1a38
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-04-11 07:50:20 +0000

    [Add]enumの設定追加

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,20 @@
 class Diary < ApplicationRecord
+  enum appetite: {
+    a_little: 0, # 少ない
+    slightly_less: 1, # やや少ない
+    usually: 2, # 普通
+    good: 3, # やや多い
+    excellent: 4, # 多い
+  }
+
+  enum excreta: {
+    a_little: 0, # 少ない
+    slightly_less: 1, # やや少ない
+    usually: 2, # 普通
+    good: 3, # やや多い
+    excellent: 4, # 多い
+  }
+
   belongs_to :user
   has_many :photos, dependent: :destroy
   has_many :strolls, dependent: :destroy
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,9 @@
 class Dog < ApplicationRecord
+  enum dog_type: {
+    large_scale_dog: 0, # 大型犬
+    medium_size_dog: 1, # 中型犬
+    small_size_dog: 2 # 小型犬
+  }
+
   belongs_to :user
 end
