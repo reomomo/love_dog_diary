269f8c6e9334eae76d305e2d6150960d351825e7
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-04-11 06:54:48 +0000

    [Add]アソシエーションの追加

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,2 +1,5 @@
 class Diary < ApplicationRecord
+  belongs_to :user
+  has_many :photos, dependent: :destroy
+  has_many :strolls, dependent: :destroy
 end
--- app/models/stroll.rb
+++ app/models/stroll.rb
@@ -1,2 +1,4 @@
 class Stroll < ApplicationRecord
+  belongs_to :diary
+  has_many :pins, dependent: :destroy
 end
