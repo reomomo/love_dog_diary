ed0e151b61eaa43a135935f0ce3a843ca248ce14
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-04-11 08:37:40 +0000

    [Add]userコントローラの追加、各ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
-  get 'strolls/new'
-  get 'strolls/index'
-  get 'strolls/edit'
-  get 'photos/index'
-  get 'photos/show'
-  get 'photos/edit'
-  get 'diaries/new'
-  get 'diaries/index'
-  get 'diaries/show'
-  get 'diaries/edit'
-  get 'my_dogs/new'
-  get 'my_dogs/edit'
-  get 'homes/top'
-  get 'homes/about'
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  get '/my_page' => 'users#show'
+  get '/information/edit' => 'users#edit'
+  patch '/information' => 'users#update'
+  get '/unsubscribe' => 'users#unsubscribe'
+
+  resources :my_dogs, only: [:new, :create, :edit, :update]
+  resources :diaries, only: [:new, :create, :index, :show, :edit, :update]
+  resources :strolls, only: [:new, :create, :index, :edit, :update, :destroy]
+  resources :photos, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :pins, only: [:create, :update, :destroy]
+
 end
