542b788efb8255824f438ae7f8a78ed76af08d28
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-04-22 02:43:29 +0000

    [Update]バリデーションの追加、エラー文の日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
   has_many :my_dogs, dependent: :destroy
   has_many :photos, dependent: :destroy
   has_many :diaries, dependent: :destroy
   has_many :strolls, dependent: :destroy
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :user_name, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
 
 end
