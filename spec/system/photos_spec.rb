814d6489ffc16f7fcc7d79cc80876890792d8c5c
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-05-09 04:35:24 +0000

    [Finish]モデルのRspecテスト終了

---
+++ spec/system/photos_spec.rb
@@ -0,0 +1,11 @@
+# 添付する画像を定義する
+image_path = Rails.root.join('spec/factories/no-image.jpg')
+
+# 画像選択フォームに画像を添付する
+attach_file('desk[image]', image_path, make_visible: true)
+
+drscribe "画像のテスト" do
+  it "添付したテスト用の画像が画面に存在するか" do
+    exoect(page).to have_selector("img[src$='no-image.jpg']")
+  end
+end
