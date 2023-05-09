84a5fcf492265f90da0ef1d1d404690004d16106
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-05-09 08:03:40 +0000

    [Update]systemのRspec確認中

--- spec/system/my_dogs_spec.rb
+++ spec/system/my_dogs_spec.rb
@@ -1,10 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+RSpec.describe "AdminArticlesEyecatches", type: :system do
+  let!(:article) { create :article }
+  let(:user) { create :user }
 # 添付する画像を定義する
-image_path = Rails.root.join('spec/factories/no-image.jpg')
+image_path = Rails.root.join('system/no-image.jpg')
 
 # 画像選択フォームに画像を添付する
-attach_file('desk[image]', image_path, make_visible: true)
-
-drscribe "画像のテスト" do
+# describe "画像のテスト" do
+  before do
+    user = create(:user)
+    login_as(user)
+    visit articles_path
+    attach_file('desk[image]', image_path, make_visible: true)
+  end
   it "添付したテスト用の画像が画面に存在するか" do
     exoect(page).to have_selector("img[src$='no-image.jpg']")
   end
--- spec/system/photos_spec.rb
+++ spec/system/photos_spec.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
 # 添付する画像を定義する
-image_path = Rails.root.join('spec/factories/no-image.jpg')
+image_path = Rails.root.join('system/no-image.jpg')
 
 # 画像選択フォームに画像を添付する
 attach_file('desk[image]', image_path, make_visible: true)
