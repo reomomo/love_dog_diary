56721b5c2f9170112aaf472c2fc105884ef19f2f
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-05-09 14:16:08 +0000

    [Update]RSpec画像登録の確認テストコード調整中

--- spec/system/diaries_spec.rb
+++ spec/system/diaries_spec.rb
@@ -5,10 +5,10 @@ require "rails_helper"
 describe "日記保存のテスト" do
   before do
     @user = create(:user)
+    sign_in(@user)
     @my_dog = create(:my_dog, user_id: @user.id)
   end
 
-  let!(:diary) { create(:diary, user_id: @user.id, my_dog_id: @my_dog.id, diary_date: "2023年5月7日", memo: "涼しくてよく歩いた", appetite: "usually",excreta: "usually") }
   describe "トップ画面(root_path)のテスト" do
     before do
       visit root_path
@@ -22,71 +22,72 @@ describe "日記保存のテスト" do
       end
     end
   end
+  let!(:diary) { create(:diary, user_id: @user.id, my_dog_id: @my_dog.id, diary_date: "2023年5月7日", memo: "涼しくてよく歩いた", appetite: "usually",excreta: "usually") }
 
   # describe "一覧画面のテスト" do
   #   before do
   #     visit diaries_path
   #   end
   #   context "リンクの確認" do
-      # it "日記の新規登録のリンクが表示されるか" do
-      #   expect(page).to have_link "", href: new_diary_path
-      # end
-      # it "日記の詳細画面へのリンクが表示されるか" do
-      #   expect(page).to have_current_path diary_path(diary)
-      # end
+  #     it "日記の新規登録のリンクが表示されるか" do
+  #       expect(page).to have_link "", href: new_diary_path
+  #     end
+  #     it "日記の詳細画面へのリンクが表示されるか" do
+  #       expect(page).to have_current_path diary_path(diary)
+  #     end
   #   end
   # end
 
-  # describe "日記保存画面のテスト" do
-  #   before do
-  #     visit new_diary_path
-  #   end
-  #   context "表示の確認" do
+  describe "日記保存画面のテスト" do
+    before do
+      visit new_diary_path
+    end
+    context "表示の確認" do
       # it 'new_diary_pathが"/diaries/new"を含むURLか' do
       #   "current_path".should include "/diaries/new"
       # end
-      # it "保存ボタンが表示される" do
-      #   expect(page).to have_button "保存する"
-      # end
-  #   end
-  # end
+      it "保存ボタンが表示される" do
+        expect(page).to have_button "保存する"
+      end
+    end
+  end
 
   # describe "一覧画面のテスト" do
   #   before do
   #     visit diaries_path
   #   end
-    # context "一覧の表示の確認" do
-      # it "保存した日記がカレンダーに表示されているか" do
-      #   expect(page).to have_field "diary[appetite]"
-      #   expect(page).to have_field "diary[excreta]"
-      # end
-    # end
+  #   context "一覧の表示の確認" do
+  #     it "保存した日記がカレンダーに表示されているか" do
+  #       # expect(page).to have_field "diary[appetite]"
+  #       expect(page).to have_field "diary[excreta]"
+  #     end
+  #   end
   # end
 
-  # describe "編集画面のテスト" do
-  #   before do
-  #     visit edit_diary_path(diary)
-  #   end
-  #   context "表示の確認" do
-  #     it "編集前のメモ、食欲、排せつが表示されている" do
-  #       expect(page).to have_field "diary[memo]", with: diary.memo
-  #       expect(page).to have_field "diary[appetite]", with: diary.appetite
-  #       expect(page).to have_field "diary[excreta]", with: diary.excreta
-  #     end
-  #     it "変更するボタンが表示される" do
-  #       expect(page).to have_button "変更する"
-  #     end
-  #   end
-  #   context "更新処理に関するテスト" do
-  #     it "更新後のリダイレクト先は正しいか" do
-  #       fill_in "diary[memo]",with: Faker::Lorem.characters(number:20)
-  #       fill_in "diary[appetite]", "usually"
-  #       fill_in "diary[excreta]", "usually"
-  #       click_button "変更する"
-  #       expect(page).to have_current_path diary_path(diary)
-  #     end
-  #   end
-  # end
+  describe "編集画面のテスト" do
+    before do
+      visit edit_diary_path(diary)
+    end
+    context "表示の確認" do
+      it "編集前のメモ、食欲、排せつが表示されている" do
+        expect(page).to have_field "diary[memo]", with: diary.memo
+        expect(page).to have_field "diary[appetite]", with: diary.appetite
+        expect(page).to have_field "diary[excreta]", with: diary.excreta
+      end
+      it "変更するボタンが表示される" do
+        expect(page).to have_button "変更する"
+      end
+    end
+    # context "更新処理に関するテスト" do
+    #   it "更新後のリダイレクト先は正しいか" do
+    #     fill_in "diary[memo]",with: Faker::Lorem.characters(number:20)
+    #     fill_in "diary[appetite]", "usually"
+    #     fill_in "diary[excreta]", "usually"
+    #     click_button "変更する"
+    #     expect(page).to have_current_path diary_path(diary)
+    #   end
+    # end
+  end
 end
 
 
--- spec/system/my_dogs_spec.rb
+++ spec/system/my_dogs_spec.rb
@@ -1,21 +1,30 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-RSpec.describe "AdminArticlesEyecatches", type: :system do
-  let!(:article) { create :article }
-  let(:user) { create :user }
-# 添付する画像を定義する
-image_path = Rails.root.join('system/no-image.jpg')
 
-# 画像選択フォームに画像を添付する
-# describe "画像のテスト" do
+RSpec.describe "Mydog", type: :system do
+  # let(:user) { create :user }
+  # let(:my_dog) { create :my_dog }
+
+  # 添付する画像を定義する
+  image_path = Rails.root.join('spec/system/no-image.jpg')
+
+  # 画像選択フォームに画像を添付する
   before do
-    user = create(:user)
-    login_as(user)
-    visit articles_path
-    attach_file('desk[image]', image_path, make_visible: true)
+    @user = create(:user)
+    sign_in(@user)
+    visit new_my_dog_path
+    # @my_dog = FactoryBot.build(:user)
+    # @my_dog = create(:my_dog, user_id: @user.id, name: "レオ", size: "small_size_dog")
+    fill_in 'user_id', with: @user.id
+    fill_in 'name', with: "レオ"
+    fill_in 'size', with: "small_size_dog"
+    attach_file('my_dog[image]', image_path, make_visible: true)
+    click_button '登録する'
+    byebug
+    # visit my_page_path
   end
   it "添付したテスト用の画像が画面に存在するか" do
-    exoect(page).to have_selector("img[src$='no-image.jpg']")
+    expect(page).to have_selector("img[src$='no-image.jpg']")
   end
 end
