e7cece766dceacefc9a5af1520293e6ec94f7319
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-06-21 13:01:55 +0000

    カレンダーもタブメニューに変更

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
-  $('#tab-contents .tab[id != "tab1"]').hide();
-})
+$(document).on('turbolinks:load',function() {
+  $('#tab-contents .tab[id != "tab0"]').hide();
+});
 
-$(document).ready(function() {
+$(document).on('turbolinks:load',function() {
   $('#tab-menu a').on('click', function(event){
     $("#tab-contents .tab").hide();
     $("#tab-menu .active").removeClass("active");
@@ -10,18 +10,4 @@ $(document).ready(function() {
     $($(this).attr("href")).show();
     event.preventDefault();
   });
-})
-
-$(document).ready(function() {
-  $('#tab-contents1 .tab[id != "tab1"]').hide();
-})
-
-$(document).ready(function() {
-  $('#tab-menu1 a').on('click', function(event){
-    $("#tab-contents1 .tab").hide();
-    $("#tab-menu1 .active").removeClass("active");
-    $(this).addClass("active");
-    $($(this).attr("href")).show();
-    event.preventDefault();
-  });
-})
+});
